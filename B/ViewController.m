109b45033d05a46d97907afced6d22cd29a5902b
jiangjiawen <jiawen.jiang@bkjk.com>
2018-03-22 16:42:02 +0800

    first commit

---
+++ B/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  B
+//
+//  Created by jiangjiawen on 2018/3/22.
+//  Copyright © 2018年 bkjk. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
